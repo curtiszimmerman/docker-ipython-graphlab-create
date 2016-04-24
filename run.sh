5d9bf323c7052974e5c48cf66b00a1813ecf2542
curtiszimmerman <curtis.zimmerman@gmail.com>
2016-04-24 03:18:54 -0700

    Use host network stack (random port assigned to GraphLab Create)

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # if you DID NOT enable nvidia-docker
-#docker run -d --name="graphlab" -v "`pwd`/data:/data" -p 8888:8888 -p 12000:12000 graphlab:latest
+#docker run -d --name="graphlab" -v "`pwd`/data:/data" --net=host graphlab:latest
 
 # or to run with the special GPU-enabled nvidia-docker
-nvidia-docker run -d --name="graphlab" -v="`pwd`/data:/data" -p 8888:8888 -p 12000:12000 graphlab:latest
+nvidia-docker run -d --name="graphlab" -v="`pwd`/data:/data" --net=host graphlab:latest
